dd8e831463d0d5ebbd34e6628d58e047321eb1a4
AS <asadrgilany7@gmail.com>
2021-10-29 17:39:32 -0400

    connected mysql to node

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require("mysql2");
 
 // express middleware
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to the database
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        // sql username
+        user: "root",
+        // password
+        password: "password123!",
+        database: "election"
+    },
+    console.log("Connected to the election database.")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 app.use((req, res) => {
     res.status(404).end();
